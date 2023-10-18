7a1481d53cf6d3fa641fced87f14f524f9f44eee
Vick7 <phamhieu16082002@gmail.com>
2023-10-18 09:37:45 +0700

    update something

--- todo-admin-app/src/dataProvider.ts
+++
@@ -1,7 +0,0 @@
-import jsonServerProvider from "ra-data-json-server";
-import { DataProvider, fetchUtils } from "react-admin";
-import { stringify } from "querystring";
-
-export const dataProvider = jsonServerProvider(
-  import.meta.env.VITE_JSON_SERVER_URL
-);
